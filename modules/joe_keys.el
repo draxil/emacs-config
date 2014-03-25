59d04b53f74e4bb2978d82cbaef376492aafc207
draxil <draxil@gmail.com>
2014-03-25 11:51:58 +0000

    Some comments to make my keys more discoverable

--- modules/joe_keys.el
+++ modules/joe_keys.el
@@ -1,4 +1,6 @@
 (global-set-key (kbd "C-c i") 'indent-region)
 (global-set-key (kbd "C-c a") 'mark-whole-buffer)
+;; My key before this had a sensible default in emacs:
 (global-set-key (kbd "C-c g") 'goto-line)
+;; "go back to where I just was" - Invaluable!
 (global-set-key (kbd "C-c b") 'View-back-to-mark)
