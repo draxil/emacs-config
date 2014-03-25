7bd894f82f9d11430619cdaa7175e0779ed65022
draxil <draxil@gmail.com>
2014-03-25 11:48:51 +0000

    Added extra keys:
    My version of goto line: because it turns out my brain can't change
    View back to mark: because I can't live without that!

--- modules/joe_keys.el
+++ modules/joe_keys.el
@@ -1,2 +1,4 @@
 (global-set-key (kbd "C-c i") 'indent-region)
 (global-set-key (kbd "C-c a") 'mark-whole-buffer)
+(global-set-key (kbd "C-c g") 'goto-line)
+(global-set-key (kbd "C-c b") 'View-back-to-mark)
