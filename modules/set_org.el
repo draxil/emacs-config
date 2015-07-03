eee611090a8a0296ddb913efd841f17814120a55
draxil <draxil@gmail.com>
2015-07-03 11:48:06 +0100

    some stolen org-mode snippet

---
+++ modules/set_org.el
@@ -0,0 +1,14 @@
+
+;; http://whattheemacsd.com//setup-org.el-01.html
+(defun myorg-update-parent-cookie ()
+  (when (equal major-mode 'org-mode)
+    (save-excursion
+      (ignore-errors
+        (org-back-to-heading)
+        (org-update-parent-todo-statistics)))))
+
+(defadvice org-kill-line (after fix-cookies activate)
+  (myorg-update-parent-cookie))
+
+(defadvice kill-whole-line (after fix-cookies activate)
+  (myorg-update-parent-cookie))
