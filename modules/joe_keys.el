59df3f375f085bf0c90e91a70fbdd294027386dc
draxil <draxil@gmail.com>
2015-10-29 17:01:12 +0000

    Changes to facilitate use of org-mode agenda..

--- modules/joe_keys.el
+++ modules/joe_keys.el
@@ -1,7 +1,6 @@
 ;;; General customised keys that don't fit in a particular file somewhere
 
 (global-set-key (kbd "C-c i") 'indent-region)
-(global-set-key (kbd "C-c a") 'mark-whole-buffer)
 ;; My key before this had a sensible default in emacs:
 (global-set-key (kbd "C-c g") 'goto-line)
 ;; "go back to where I just was" - Invaluable!
