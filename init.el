f2ee4abe755e44ad960a3a3f4dcb530dcf216b00
draxil <draxil@gmail.com>
2016-03-03 11:00:04 +0000

    Added extra repos, email setting changes, org stuff

--- init.el
+++ init.el
@@ -17,8 +17,9 @@
 
 (require 'package)
 
-(add-to-list 'package-archives
-'("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
 
 (package-initialize)
 
@@ -45,7 +46,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Dropbox/org/work/work.org"))))
+ '(custom-safe-themes (quote ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(org-agenda-files (quote ("~/Dropbox/org/work/work.org")))
+ '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-rmail org-vm org-wl org-w3m org-mouse)))
+ '(sml/theme (quote dark))
+ '(smtpmail-smtp-server "office.printevolved.co.uk")
+ '(smtpmail-smtp-service 25))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
