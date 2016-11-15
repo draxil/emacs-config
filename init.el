3f13073af43303c05d010076631edb47ca147bbc
joe <draxil@gmail.com>
2016-11-15 14:49:28 +0000

    General init changes

--- init.el
+++ init.el
@@ -47,9 +47,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(org-agenda-files (quote ("~/Dropbox/org/work/work.org")))
- '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-rmail org-vm org-wl org-w3m org-mouse)))
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-mew org-mhe org-rmail org-vm org-wl org-w3m org-mouse)))
+ '(package-selected-packages
+   (quote
+    (gradle-mode yaml-mode tidy smart-mode-line sauron s perlcritic perl6-mode multi-term marmalade markdown-mode magit ido-ubiquitous go-mode gist full-ack deft android-mode alert)))
  '(sml/theme (quote dark))
  '(smtpmail-smtp-server "office.printevolved.co.uk")
  '(smtpmail-smtp-service 25))
