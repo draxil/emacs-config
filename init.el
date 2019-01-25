b29811df38283a1b057389cab837016f28222ac1
joe <draxil@gmail.com>
2019-01-25 09:35:21 +0000

    package state fix

--- init.el
+++ init.el
@@ -18,8 +18,12 @@
 (require 'package)
 
 ;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+
 
 
 (package-initialize)
