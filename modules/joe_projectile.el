c2426ba97511d823ae86914732831f9d8f63eb70
joe <draxil@gmail.com>
2019-10-16 14:49:38 +0100

    diminish projectile mode

--- modules/joe_projectile.el
+++ modules/joe_projectile.el
@@ -5,4 +5,6 @@
     (projectile-mode +1)
     (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
     (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-    ))
+    )
+  :diminish projectile-mode
+  )
