67e6f87ccc3b14cad412d552efb6d6939f52d73c
Joe Higton <draxil@gmail.com>
2021-02-18 19:23:54 +0000

    Jsx stuffs

---
+++ modules/joe_jsx.el
@@ -0,0 +1,40 @@
+;; Largely stolen, also some stuff in flycheck. So:
+;; TODO decide if it's all for keeps
+
+(defun my/activate-tide-mode ()
+  "Use hl-identifier-mode only on js or ts buffers."
+  (when (and (stringp buffer-file-name)
+             (string-match "\\.[tj]sx?\\'" buffer-file-name))
+	(tide-setup)
+  (tide-hl-identifier-mode)))
+
+(defun my/set-local-eslint ()
+  "Use local node_modules."
+  (add-node-modules-path)
+  (setq-local flycheck-javascript-eslint-executable (executable-find "eslint")))
+
+
+(use-package tide
+  :ensure t
+  :config
+  (flycheck-add-next-checker 'tsx-tide 'javascript-eslint)
+  :hook (web-mode . my/activate-tide-mode))
+
+(use-package web-mode
+  :ensure t
+  :mode
+  ("\\.ejs\\'" "\\.hbs\\'" "\\.html\\'" "\\.php\\'" "\\.[jt]sx?\\'")
+  :config
+  (setq web-mode-content-types-alist '(("jsx" . "\\.[jt]sx?\\'")))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-script-padding 2)
+  (setq web-mode-block-padding 2)
+  (setq web-mode-style-padding 2)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-auto-closing t)
+  (setq web-mode-enable-current-element-highlight t))
+
+(use-package add-node-modules-path
+  :ensure t)
