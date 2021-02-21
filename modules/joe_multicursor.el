c139be57a83dfd6ba52cdccd6d61dbd224cb8124
Joe Higton <draxil@gmail.com>
2021-02-21 16:02:53 +0000

    Missing MC bind

--- modules/joe_multicursor.el
+++ modules/joe_multicursor.el
@@ -3,6 +3,7 @@
 	( "C-S-c C-S-c" . 'mc/edit-lines)
 	("C-S-a C-S-a" . 'mc/edit-beginnings-of-lines)
 	("C-c C->" . 'mc/mark-all-like-this)
+	("C->" . 'mc/mark-next-like-this)
 	:map multiple-cursors-mode-hook
 	("C-?" . 'mc/unmark-next-like-this)
 	("C-<" . 'mc/mark-previous-like-this)
