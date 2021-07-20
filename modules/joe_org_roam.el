d13fcc949ef64106c5bf7d1dbcacb789289108ec
Joe Higton <draxil@gmail.com>
2021-07-20 10:57:47 +0100

    Org roam 2.0

--- modules/joe_org_roam.el
+++ modules/joe_org_roam.el
@@ -1,7 +1,6 @@
 (use-package org-roam
   :init
-  ;;  (add-hook 'after-init-hook (org-roam-mode))
-  (use-package org-roam-dailies)
+  (setq org-roam-v2-ack t)
   :diminish 'org-roam-mode
   :custom
   (org-roam-directory "~/Dropbox/org-roam/")
@@ -9,15 +8,16 @@
   ; also no need because it's a cache so saves pointless syncs.
   (org-roam-db-location "~/.local/my/org-roam/org-roam.db")
   :config
+  (org-roam-setup)
   :bind (
-	 ("C-<f9>" . org-roam-find-file)
-	 ("<f5>". org-roam-dailies-today)
-	 ("M-<f5>". org-roam-dailies-yesterday)
+	 ("C-<f9>" . org-roam-node-find)
+	 ("<f5>". org-roam-dailies-goto-today)
+	 ("M-<f5>". org-roam-dailies-goto-yesterday)
 	 :map org-roam-mode-map
 	 (("C-c n l" . org-roam)
-	  ("C-c n f" . org-roam-find-file)
-	  ("C-c n g" . org-roam-graph-show))
+	  ("C-c n f" . org-roam-node-find)
+	  ("C-c n g" . org-roam-graph))
 	 :map org-mode-map
-	 (("C-c n i" . org-roam-insert))
+	 (("C-c n i" . org-roam-node-insert))
 	 (("C-c n I" . org-roam-insert-immediate))
 	 ))
