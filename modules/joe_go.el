f4f53097a78ba283b70f5abd495f507b218443f8
Joe Higton <draxil@gmail.com>
2021-08-08 13:58:07 +0100

    Go updates

--- modules/joe_go.el
+++ modules/joe_go.el
@@ -2,8 +2,10 @@
 
 (use-package go-mode
   :config
+  (add-hook 'before-save-hook #'gofmt-before-save)
+  (setq-local show-trailing-whitespace t)
   :bind (
 	 :map go-mode-map
-	      (("C-c t t" . go-test-current-file))
+	      (("C-c t t" . go-test-current-project))
 	      (("C-c t f" . go-test-current-test))
   ))
