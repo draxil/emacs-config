cae384736a878ba1b6afb7325a64021cbd722932
Joe Higton <draxil@gmail.com>
2021-08-09 17:21:31 +0100

    Flycheck update

--- modules/joe_flycheck.el
+++ modules/joe_flycheck.el
@@ -1,8 +1,25 @@
 (use-package flycheck
   :defer 2
   :init
-  :hook (web-mode . my/set-local-eslint)
+;  :hook ;;(web-mode . my/set-local-eslint)
   :config
   (flycheck-add-mode 'javascript-eslint 'web-mode)
   (setq flycheck-check-syntax-automatically '(mode-enabled save))
-  (global-flycheck-mode))
+  (global-flycheck-mode)
+  (setq-default flycheck-disabled-checkers
+      (append flycheck-disabled-checkers
+	      '(javascript-jshint)))
+  (flycheck-add-mode 'javascript-eslint 'web-mode)
+
+  ;; solution for using local eslint?!
+  (defun my/use-eslint-from-node-modules ()
+    (let* ((root (locate-dominating-file
+                  (or (buffer-file-name) default-directory)
+                  "node_modules"))
+           (eslint (and root
+			(expand-file-name "node_modules/eslint/bin/eslint.js"
+                                          root))))
+      (when (and eslint (file-executable-p eslint))
+	(setq-local flycheck-javascript-eslint-executable eslint))))
+  (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+  )
