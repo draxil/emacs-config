1ee453556ab0ee66d8043ccc8b9cf0e041337366
Joe Higton <draxil@gmail.com>
2021-11-28 12:05:53 +0000

    notmuch patch for vertico

--- modules/joe_vertico.el
+++ modules/joe_vertico.el
@@ -21,7 +21,11 @@
   ;; This enables consults enhanced competing read multiple.
   ;; honestly I'm not sure I'm getting any use of this yet?
   (advice-add #'completing-read-multiple
-            :override #'consult-completing-read-multiple)
+              :override #'consult-completing-read-multiple)
+
+  ;; workaround for notmuch tag issue
+  (advice-add #'notmuch-read-tag-changes :filter-return (lambda (x) (mapcar #'string-trim x)))
+  
   
   ;; Grow and shrink the Vertico minibuffer
   ;;   (setq vertico-resize t)
