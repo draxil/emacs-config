cae63346e11a25321eb5c61f0b30587fd39694d3
Joe Higton <draxil@gmail.com>
2021-12-15 16:36:43 +0000

    golang: flyspell + some useful go mod funcs

--- modules/joe_go.el
+++ modules/joe_go.el
@@ -5,7 +5,9 @@
   (setq-local show-trailing-whitespace t)
   :hook
   (go-mode . (lambda()
-	       (display-line-numbers-mode)))
+	       (display-line-numbers-mode)
+	       (flyspell-prog-mode)
+	       ))
   :bind (
 	 :map go-mode-map
 	      (("C-c t t" . go-test-current-project))
@@ -15,3 +17,12 @@
   (interactive)
   (shell-command "bash -c 'go generate .'" "*go generate*"))
 
+(defun joe-go-mod-init (arg)
+  "run go mod init"
+  (interactive "spackage: ")
+  (shell-command (concat "go mod init " arg) "*go mod*"))
+
+(defun joe-go-mod-tidy ()
+  "run go mod tidy"
+  (interactive)
+  (shell-command "go mod tidy" "*go mod*"))
