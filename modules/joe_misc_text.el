9ae82de5cb7828f35a359ecc51ef164bbc2ebdf2
Joe Higton <draxil@gmail.com>
2022-02-22 16:15:19 +0000

    more random text functions

--- modules/joe_misc_text.el
+++ modules/joe_misc_text.el
@@ -20,3 +20,28 @@
           (while (re-search-forward "\\\\\"" nil t)
             (replace-match "\""))))
     (print "This function operates on a region")))
+
+;;; todo obey the spaces setting for prog mode
+(defun joe-tabs-to-spaces-region (beg end)
+  "tabs to spaces"
+  (interactive "r")
+    (if (use-region-p)
+      (save-excursion
+        (save-restriction
+          (narrow-to-region beg end)
+          (goto-char (point-min))
+          (while (re-search-forward "	" nil t)
+            (replace-match "    "))))
+    (print "This function operates on a region")))
+(defun joe-nbsp-to-sp-region (beg end)
+  "tabs to spaces"
+  (interactive "r")
+    (if (use-region-p)
+      (save-excursion
+        (save-restriction
+          (narrow-to-region beg end)
+          (goto-char (point-min))
+          (while (re-search-forward " " nil t)
+            (replace-match " "))))
+    (print "This function operates on a region")))
+
