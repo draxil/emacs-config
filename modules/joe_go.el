f34ac1e6fafd07bc76ff3d0ba5cca79097170007
Joe Higton <draxil@gmail.com>
2022-03-19 17:16:06 +0000

    Add my fork of ginkgo

--- modules/joe_go.el
+++ modules/joe_go.el
@@ -50,3 +50,8 @@
   "get the current package into the kill-ring"
   (interactive)
   (kill-new (chomp (shell-command-to-string "go list"))))
+
+(use-package ginkgo-mode
+  :straight (ginkgo-mode :type git :host github :repo "draxil/ginkgo-mode")
+  :custom
+  (ginkgo-flags "-tags=unit" "run unit tests by default"))
