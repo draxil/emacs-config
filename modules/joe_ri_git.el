371eabcd524abbd1bc8eb3e3d4af92176cf184e9
Joe Higton <draxil@gmail.com>
2022-04-06 18:58:06 +0100

    [ri] git stuff

--- modules/joe_ri_git.el
+++ modules/joe_ri_git.el
@@ -1,16 +1,20 @@
 (defun ri-new-injector-branch (name)
     (interactive "sName: ")
-    (ri-new-branch name  (ri-repo-path "product-injector-api")))
+    (ri-new-branch name  "product-injector-api"))
 
 (defun ri-new-backbone-branch (name)
     (interactive "sName: ")
-    (ri-new-branch name  (ri-repo-path "product-backbone-v2")))
+    (ri-new-branch name  "product-backbone-v2"))
+
+(defun ri-new-product-teraform-branch (name)
+    (interactive "sName: ")
+    (ri-new-branch name  "product-terraform-v2"))
 
 (defun ri-repo-path (which)
   (concat joe-code-root "ri/" which))
 
 (defun ri-new-branch (name repo)
-     (magit repo)
+     (magit (ri-repo-path repo))
     (magit-call-git "fetch" "origin" "master")
     (magit-call-git "branch" name "origin/master")
     (magit-checkout name))
