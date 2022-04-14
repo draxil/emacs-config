fdac8b6d52ea4706549879a4bad4b3f067169ec8
Joe Higton <draxil@gmail.com>
2022-04-14 10:43:49 +0100

    yasnippet: move trigger key because tab can be ambiguous.

--- modules/joe_yasnippet.el
+++ modules/joe_yasnippet.el
@@ -3,4 +3,9 @@
   :diminish 'yas-minor-mode
   :config
   (yas-global-mode 1)
+  :bind (:map yas-minor-mode-map
+	     ("<backtab>" . yas-expand)
+	     ("TAB" . nil)
+	     ("<tab>" . nil)
+	     )
   )
