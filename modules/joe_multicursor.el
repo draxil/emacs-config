c2f17f7260780ca3aea0fe672a4476951f44668d
Joe Higton <draxil@gmail.com>
2022-07-12 09:43:00 +0100

    [multicursor] keys for skip and search

--- modules/joe_multicursor.el
+++ modules/joe_multicursor.el
@@ -5,8 +5,14 @@
 	("C-S-a C-S-a" . 'mc/edit-beginnings-of-lines)
 	("C-c C->" . 'mc/mark-all-like-this)
 	("C->" . 'mc/mark-next-like-this)
-	:map multiple-cursors-mode-hook
-	("C-?" . 'mc/unmark-next-like-this)
-	("C-<" . 'mc/mark-previous-like-this)
-	)
+	:map multiple-cursors-mode
+	(("C-?" . 'mc/unmark-next-like-this)
+	 ("C-c C-?".'mc/skip-to-next-like-this)
+	 ("C-<" . 'mc/mark-previous-like-this)
+	 ("C-S-s" . 'phi-search)
+	 ("C-S-r" . 'phi-search-backward)))
   )
+
+;; purely for mc at the moment, but see if it's okay to replace?
+(use-package phi-search
+ :straight t)
