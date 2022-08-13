bc3d756cc1b21dce2dba62863d6022abfe5b6a69
Joe Higton <draxil@gmail.com>
2022-08-13 08:56:02 +0100

    tweak to center fill mode

--- modules/joe_centre_fill.el
+++ modules/joe_centre_fill.el
@@ -1,7 +1,7 @@
 
 (define-minor-mode joe-centre-fill-mode
   "minor mode to do center and fill"
-  :version 1
+  :init-value nil
   (progn
     (visual-fill-column-mode 'toggle)
     (setq-local visual-fill-column-width 80)
