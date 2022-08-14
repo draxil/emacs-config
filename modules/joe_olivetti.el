5dc30d59aca50550b88f0ec8552828ac0e9e48c1
Joe Higton <draxil@gmail.com>
2022-08-14 14:21:00 +0100

    [olivetti] no fringes

--- modules/joe_olivetti.el
+++ modules/joe_olivetti.el
@@ -1,2 +1,10 @@
 (use-package olivetti
-  :straight t)
+  :straight t
+  :hook (
+	 (olivetti-mode-on . (lambda()
+			       (visual-line-mode)
+			       (set-fringe-style 0)
+			       ))
+  	 (olivetti-mode-off . (lambda()
+				(set-fringe-style nil)
+			       ))))
