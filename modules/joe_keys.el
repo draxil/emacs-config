6e13062a53e64c0353704bf5a9ee9759e465e892
Joe Higton <draxil@gmail.com>
2022-11-08 10:26:12 +0000

    [keys] more useful "upcase" bind

--- modules/joe_keys.el
+++ modules/joe_keys.el
@@ -49,6 +49,8 @@
 (bind-key "C-c j j" 'ace-window)
 ;; for when we explicitly don't want it in our kill ring
 (bind-key "C-S-w" 'delete-region)
+;; replace upcase-word with DWIM as it's much more flexible
+(bind-key "M-u" 'upcase-dwim)
 
 ;; for when we want eshell powers, perhaps make it pop like vterm at some point?
 (bind-key "M-<f2>" 'eshell)
