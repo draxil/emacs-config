da05e7e8545b1b41f353ea1f4116ced2a4b017b6
Joe Higton <draxil@gmail.com>
2023-01-06 17:33:08 +0000

    [ri] api run tweak

--- modules/joe_ri.el
+++ modules/joe_ri.el
@@ -146,5 +146,5 @@
 
 (defun ri-inj-run-api ()
   (interactive)
-  (async-shell-command "cd `git rev-parse --show-toplevel` && make run/refresh-api && docker logs -f product-injector-api-injector-api-1" "*ri-inj-api*"))
+  (async-shell-command "cd `git rev-parse --show-toplevel` && make run/refresh-api && docker logs --tail=1 -f product-injector-api-injector-api-1" "*ri-inj-api*"))
 
