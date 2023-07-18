9793b0bcf46a61a5079348e22b1fa1cfadfbe520
Joe Higton <draxil@gmail.com>
2023-07-18 11:11:26 +0100

    [yasnippet] auto install yasnippet, propogate crude fix

--- modules/joe_yasnippet.el
+++ modules/joe_yasnippet.el
@@ -4,9 +4,18 @@
   :diminish 'yas-minor-mode
   :config
   (yas-global-mode 1)
+
+  ;; SUPER CRUDE PATCH for yasnippet suddenly being annoying! 
+  (define-key yas-keymap [(tab)]        nil)
+  (define-key yas-keymap (kbd "TAB")    nil)
+  (define-key yas-keymap (kbd "<tab>")  nil)
+
   :bind (:map yas-minor-mode-map
 	     ("C-<tab>" . yas-expand)
 	     ("TAB" . nil)
 	     ("<tab>" . nil)
 	     )
   )
+(use-package yasnippet-snippets
+  :straight t
+  :after yasnippet)
