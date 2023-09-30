2b8a18f5069ed12b8a4d0e206c3d8db2f2312993
Joe Higton <draxil@gmail.com>
2023-09-30 16:31:15 +0100

    [yasnippet] I think we can ditch our messy fix?

--- modules/joe_yasnippet.el
+++ modules/joe_yasnippet.el
@@ -1,21 +1,15 @@
 (use-package yasnippet
   :straight t
-  :defer 2
-  :diminish 'yas
+  :diminish 'yas-minor-mode
   :config
   (yas-global-mode 1)
 
-  ;; SUPER CRUDE PATCH for yasnippet suddenly being annoying! 
-  (define-key yas-keymap [(tab)]        nil)
-  (define-key yas-keymap (kbd "TAB")    nil)
-  (define-key yas-keymap (kbd "<tab>")  nil)
-
-  :bind (:map yas-minor-mode-map
+  :bind(
+	:map yas-minor-mode-map ( 
 	     ("C-<tab>" . yas-expand)
 	     ("TAB" . nil)
 	     ("<tab>" . nil)
-	     )
-  )
+  ))
 (use-package yasnippet-snippets
   :straight t
   :after yasnippet)
