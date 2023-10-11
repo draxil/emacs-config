2ca0721e2b9aa9243ab979239f0803c64138fb8d
Joe Higton <draxil@gmail.com>
2023-10-11 13:03:54 +0100

    [work] quick simple org cleanup function

--- modules/joe_work.el
+++ modules/joe_work.el
@@ -4,3 +4,13 @@
 
 ;; VERY PROVISIONAL, move key to keys?
 (global-set-key (kbd "C-x w") 'capture-work)
+
+(defun joe-work-org-archive-done-tasks ()
+  (interactive)
+  (org-map-entries
+   (lambda ()
+     (let ((org-archive-location "work.org::* *RECENT*"))
+       (org-archive-subtree)
+       (setq org-map-continue-from (org-element-property :begin (org-element-at-point)))))
+   "/DONE" 'file)
+  (org-save-all-org-buffers))
