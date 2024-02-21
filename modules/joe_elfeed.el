d54b143c3523d787da3cd73908e673af7bf2d6f6
Joe Higton <draxil@gmail.com>
2024-02-21 17:28:24 +0000

    somewhat fix my elfeed

--- modules/joe_elfeed.el
+++ modules/joe_elfeed.el
@@ -1,17 +1,15 @@
 (use-package elfeed
   :straight t
   :custom
-    (elfeed-db-directory "~/Nextcloud/elfeed")
+    (elfeed-db-directory "~/Nextcloud/elfeed"))
+(use-package elfeed-org
+  :straight t
   :init
-  (use-package elfeed-org
-    :straight t
-    :init
-    (elfeed-org)
-    (setq rmh-elfeed-org-files (list "~/Nextcloud/org/elfeed.org"))
-    :hook
-    (elfeed-db-update . (lambda ()
-			  (message "doing elfeed hook")
-			  (elfeed-db-save)
-			  ))
-    ))
- 
+  (elfeed-org)
+  (setq rmh-elfeed-org-files (list "~/Nextcloud/org/elfeed.org"))
+  :hook
+  (elfeed-db-update . (lambda ()
+			(message "doing elfeed hook")
+			(elfeed-db-save)
+			))
+  )
