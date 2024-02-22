174e96c3d855bf1bd518633d0a912c2e2c616300
Joe Higton <draxil@gmail.com>
2024-02-22 08:41:19 +0000

    [file] make exe command

--- modules/joe_file.el
+++ modules/joe_file.el
@@ -1,4 +1,10 @@
 (defun joe-chmod-file ()
   "chmod the current buffer file"
   (interactive)
-  (chmod (buffer-file-name) (read-file-modes "perms: " (buffer-file-name) )))
+  (chmod
+   (buffer-file-name) (read-file-modes "perms: " (buffer-file-name))))
+
+(defun joe-make-script-exe ()
+  "chmod +x the file if a script"
+  (interactive)
+  (executable-make-buffer-file-executable-if-script-p))
