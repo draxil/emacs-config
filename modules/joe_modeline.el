c40de26d7f328d76dba8fb0be7cf12fd7396b5d4
Joe Higton <draxil@gmail.com>
2024-02-22 22:35:01 +0000

    [modeline] more..

--- modules/joe_modeline.el
+++ modules/joe_modeline.el
@@ -1,16 +1,13 @@
 (setq-default mode-line-format
-              '("%e"
-                mode-line-front-space
+              '("%e" mode-line-front-space
                 (:propertize
                  (""
                   mode-line-mule-info
                   mode-line-client
                   mode-line-modified))
-                mode-line-frame-identification
-                "%b"
-                "   "
-                "%l/%c %n "
-                (vc-mode vc-mode)
-                "  "
-                mode-line-misc-info
-                mode-line-end-spaces))
+                mode-line-frame-identification "%b" "   "
+                "%l/%c" ;; line / column
+                " %n " ;; narrrow indicator
+                (vc-mode vc-mode) ;; vc info
+                "  " mode-line-misc-info mode-line-end-spaces))
+;; TODO: hmm no major mode info?
