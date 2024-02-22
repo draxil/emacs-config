f815cc3838b92e3de34bdf451dad1802de7d95c9
Joe Higton <draxil@gmail.com>
2024-02-22 08:39:58 +0000

    [elisp] add autofmt package

--- modules/joe_elisp_util.el
+++ modules/joe_elisp_util.el
@@ -1,7 +1,8 @@
 ;; courtesy of https://www.emacswiki.org/emacs/ElispCookbook
 (defun chomp (str)
   "Chomp leading and tailing whitespace from STR."
-  (while (string-match "\\`\n+\\|^\\s-+\\|\\s-+$\\|\n+\\'"
-                       str)
+  (while (string-match "\\`\n+\\|^\\s-+\\|\\s-+$\\|\n+\\'" str)
     (setq str (replace-match "" t t str)))
   str)
+
+(use-package elisp-autofmt :straight t)
