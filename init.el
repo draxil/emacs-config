0f670ef5c191ad3b7d00566f16bf7032cf7fdc17
Joe Higton <draxil@gmail.com>
2024-02-27 18:40:19 +0000

    init.el updates, formatting added my-packages

--- init.el
+++ init.el
@@ -3,21 +3,34 @@
 ;; Adapted from stevendanna's emacs-config
 ;; https://github.com/stevendanna/emacs-config
 
+(add-hook
+ 'emacs-startup-hook
+ (lambda ()
+   (message "Emacs ready in %s with %d garbage collections."
+            (format "%.2f seconds"
+                    (float-time
+                     (time-subtract
+                      after-init-time before-init-time)))
+            gcs-done)))
+
+
 ;; get straight sorted first:
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+       (expand-file-name "straight/repos/straight.el/bootstrap.el"
+                         user-emacs-directory))
       (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-         (url-retrieve-synchronously
+        (url-retrieve-synchronously
          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
+         'silent
+         'inhibit-cookies)
       (goto-char (point-max))
-      (eval-print-last-sexp))
-    )
+      (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 (straight-use-package 'use-package)
+(straight-use-package 'org)
 
 
 ;; load vendor and custom files
@@ -25,6 +38,8 @@
   "top level emacs dir")
 (defvar vendor-dir (concat emacs-dir "vendor/")
   "Packages not yet available in ELPA")
+(defvar my-package-dir (concat emacs-dir "my-packages/")
+  "My packages")
 
 (defvar module-dir (concat emacs-dir "modules/")
   "Where the real configuration happens")
@@ -34,6 +49,7 @@
 ;; add to load path
 (add-to-list 'load-path vendor-dir)
 (add-to-list 'load-path module-dir)
+(add-to-list 'load-path my-package-dir)
 
 ;; Do use-package before modules
 (eval-when-compile
@@ -41,10 +57,9 @@
   (require 'use-package))
 
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/")
-	     '("elpa" . "https://elpa.gnu.org/packages/")
-	     )
+(add-to-list
+ 'package-archives '("melpa" . "https://melpa.org/packages/")
+ '("elpa" . "https://elpa.gnu.org/packages/"))
 
 
 (package-initialize)
