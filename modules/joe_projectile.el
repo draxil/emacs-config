e0fbe2c47bbba479ca814bcc18a56cc69210faae
Joe Higton <draxil@gmail.com>
2024-02-29 12:32:43 +0000

    [projectile] loose helm projectile, lisp format.

--- modules/joe_projectile.el
+++ modules/joe_projectile.el
@@ -1,23 +1,17 @@
-(use-package projectile
-  :straight t
-  :defer 2
-  :diminish projectile-mode
-  :init
-  (projectile-mode +1)
-  :config
-  (setq projectile-completion-system 'default)
-  (use-package helm-projectile
-    :straight t
-    )
-  :bind
-  ( :map projectile-mode-map (
-	 ("s-p" . projectile-command-map)
-	 )
-	 :map projectile-command-map (
- 				      ("a" . helm-projectile-ack)
-				      ;; this probably will replace ^^
-				      ("r" . (lambda() (interactive) (consult-ripgrep (projectile-project-root))))
-	 )
-  )
-)
-
+(use-package
+ projectile
+ :straight t
+ :defer 2
+ :diminish projectile-mode
+ :init (projectile-mode +1)
+ :config (setq projectile-completion-system 'default)
+ :bind
+ (:map
+  projectile-mode-map (("s-p" . projectile-command-map))
+  :map projectile-command-map
+  (("a" . helm-projectile-ack)
+   ;; this probably will replace ^^
+   ("r" .
+    (lambda ()
+      (interactive)
+      (consult-ripgrep (projectile-project-root)))))))
