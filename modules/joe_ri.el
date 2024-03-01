49c437227cd1d764291485a05a2b0f3b500c1f4e
Joe Higton <draxil@gmail.com>
2024-03-01 07:17:06 +0000

    [ri] formatting and new retro capture template

--- modules/joe_ri.el
+++ modules/joe_ri.el
@@ -21,7 +21,9 @@
   (ri-find-last-planning (ri-planning-anchor) (current-time)))
 
 (defun ri-find-planning (start now)
-  (if (time-less-p (ri-add-period start) now) (ri-find-planning (ri-add-period start) now) (ri-add-period start)))
+  (if (time-less-p (ri-add-period start) now)
+      (ri-find-planning (ri-add-period start) now)
+    (ri-add-period start)))
 
 (defun ri-find-last-planning (start now)
   (time-subtract (ri-next-planning) (ri-period)))
@@ -30,31 +32,29 @@
   ""
   (cl-assert
    (string=
-    (ri-date (ri-find-planning (ri-planning-anchor) (ri-planning-anchor)))
-    "2022-03-31"
-    )
-   )
-  )
+    (ri-date
+     (ri-find-planning (ri-planning-anchor) (ri-planning-anchor)))
+    "2022-03-31")))
 (defun test-ri-next-planning-from-anchor-plus-one ()
   ""
   (cl-assert
    (string=
-    (ri-date (ri-find-planning (ri-planning-anchor) (encode-time '(1 0 0 31 3 2022 nil nil nil))))
-    "2022-04-14"
-    )
-   "next anchor plus one"
-   )
-  )
+    (ri-date
+     (ri-find-planning
+      (ri-planning-anchor)
+      (encode-time '(1 0 0 31 3 2022 nil nil nil))))
+    "2022-04-14")
+   "next anchor plus one"))
 
 (defun test-ri-last-planning-from-anchor-plus-one ()
-    ""
+  ""
   (cl-assert
    (string=
-    (ri-date (ri-find-last-planning (ri-planning-anchor) (encode-time '(1 0 0 31 3 2022 nil nil nil))))
-    "2022-03-17"
-    )
-   )
-  )
+    (ri-date
+     (ri-find-last-planning
+      (ri-planning-anchor)
+      (encode-time '(1 0 0 31 3 2022 nil nil nil))))
+    "2022-03-17")))
 
 (defun test-ri-planning ()
   ""
@@ -63,63 +63,64 @@
   (test-ri-next-planning-from-anchor-plus-one)
   (cl-assert (not (string= (ri-date (ri-next-planning)) "")))
   (test-ri-last-planning-from-anchor-plus-one)
-  (cl-assert (not (string= (ri-date (ri-last-planning)) "")))
-  )
+  (cl-assert (not (string= (ri-date (ri-last-planning)) ""))))
 
 (defun next-ri-planning-org (node)
-  (concat (ri-date (ri-next-planning)) "riplanning.org")
-  )
+  (concat (ri-date (ri-next-planning)) "riplanning.org"))
 (defun next-ri-planning-title (node)
-  (ri-planning-title (ri-next-planning)
-		       ))
+  (ri-planning-title (ri-next-planning)))
 (defun ri-planning-title (date)
   (concat "Ri planning / retro: " (ri-date date)))
 
 (defun next-ri-planning-template (node)
-  (concat "#+title: " (next-ri-planning-title node) "\n#+filetags: ri ri-retro"))
+  (concat
+   "#+title: "
+   (next-ri-planning-title node)
+   "\n#+filetags: ri ri-retro"))
 (defun last-ri-planning-title ()
-  (ri-planning-title (ri-last-planning))
-  )
+  (ri-planning-title (ri-last-planning)))
 
 (defun ri-planning-retro ()
   (interactive)
-  (org-roam-node-visit (org-roam-node-from-title-or-alias (last-ri-planning-title))))
+  (org-roam-node-visit
+   (org-roam-node-from-title-or-alias (last-ri-planning-title))))
 (defun ri-planning-retro-next ()
   (interactive)
-  (org-roam-node-visit (org-roam-node-from-title-or-alias (ri-planning-title (ri-next-planning)))))
+  (org-roam-node-visit
+   (org-roam-node-from-title-or-alias
+    (ri-planning-title (ri-next-planning)))))
 
 (defun capture-next-ri-planning ()
   (interactive)
   (let ((filetemplate))
 
-    (org-roam-capture- :node (org-roam-node-create)
-		       :templates '(
-				    ("g" "good - went ri" plain "** %?"
-				     :if-new (file+head+olp
-					      "${next-ri-planning-org}"
-					      "${next-ri-planning-template}"
-					      ("good")
-					      ))
-				     ("b" "bad / problem" plain "** %?"
-				     :if-new (file+head+olp
-					      "${next-ri-planning-org}"
-					      "${next-ri-planning-template}"
-					      ("bad")
-					      ))
-				     ("l" "lesson" plain "** %?"
-				     :if-new (file+head+olp
-					      "${next-ri-planning-org}"
-					      "${next-ri-planning-template}"
-					      ("lesson")
-					      ))
-
-				     ("p" "plan" plain "** %?"
-				      :if-new (file+head+olp
-					       "${next-ri-planning-org}"
-					       "${next-ri-planning-template}"
-					      ("plan")
-					      )))
-		       )))
+    (org-roam-capture-
+     :node (org-roam-node-create)
+     :templates
+     '(("c" "continue" plain "** %?"
+        :if-new
+        (file+head+olp
+         "${next-ri-planning-org}"
+         "${next-ri-planning-template}"
+         ("continue")))
+       ("h" "change" plain "** %?"
+        :if-new
+        (file+head+olp
+         "${next-ri-planning-org}"
+         "${next-ri-planning-template}"
+         ("change")))
+       ("u" "idea" plain "** %?"
+        :if-new
+        (file+head+olp
+         "${next-ri-planning-org}"
+         "${next-ri-planning-template}"
+         ("idea")))
+       ("a" "appreciate" plain "** %?"
+        :if-new
+        (file+head+olp
+         "${next-ri-planning-org}"
+         "${next-ri-planning-template}"
+         ("appreciate")))))))
 
 
 (defun ri-make-pbb-lambda-dirs ()
@@ -131,12 +132,11 @@
 
 
 (defun ri-ticket-to-org-link ()
-    (interactive)
+  (interactive)
   (let* ((url (current-kill 0))
-	 (ticket (substring url (string-match "[^/]+$" url) (length url)))
-	 )
-    (insert (concat "[[" url "][" ticket "]]"))
-    ))
+         (ticket
+          (substring url (string-match "[^/]+$" url) (length url))))
+    (insert (concat "[[" url "][" ticket "]]"))))
 
 (defun ri-ticket-todo ()
   (interactive)
@@ -144,30 +144,34 @@
   (ri-ticket-to-org-link)
   (insert " "))
 
-(define-minor-mode
-  ri-injector-mode
+(define-minor-mode ri-injector-mode
   "minor mode for working on project injector (mostly for snippet triggers)")
 
 (defun ri-inj-run-api ()
   (interactive)
   (if (buffer-live-p (get-buffer "*ri-inj-api*"))
       (kill-buffer "*ri-inj-api*"))
-  (async-shell-command "cd `git rev-parse --show-toplevel` && make run/refresh-api && docker logs --tail=1 -f product-injector-api-injector-api-1" "*ri-inj-api*"))
+  (async-shell-command
+   "cd `git rev-parse --show-toplevel` && make run/refresh-api && docker logs --tail=1 -f product-injector-api-injector-api-1"
+   "*ri-inj-api*"))
 
 (defun ri-inj-reset-db ()
   (interactive)
-  (async-shell-command "cd `git rev-parse --show-toplevel` && make reset-db" "*ri-inj-db*"))
+  (async-shell-command
+   "cd `git rev-parse --show-toplevel` && make reset-db"
+   "*ri-inj-db*"))
 
 (defun ri-srv-run-dynamo ()
   (interactive)
   (if (buffer-live-p (get-buffer "*ri-dynamo*"))
       (kill-buffer "*ri-dynamo*"))
-  (async-shell-command "cd `git rev-parse --show-toplevel` && docker rm -f ddb-local && docker run -p 8019:8000 --restart unless-stopped -d --name ddb-local amazon/dynamodb-local -jar DynamoDBLocal.jar -sharedDb" "*ri-dynamo*")
+  (async-shell-command
+   "cd `git rev-parse --show-toplevel` && docker rm -f ddb-local && docker run -p 8019:8000 --restart unless-stopped -d --name ddb-local amazon/dynamodb-local -jar DynamoDBLocal.jar -sharedDb"
+   "*ri-dynamo*")
   (setenv "LOCAL_DDB_ENDPOINT" "http://localhost:8019"))
 
 (defun ri-pre-commit-check ()
   (interactive)
   (let ((command (format "prj-make pre-commit-check")))
     (message command)
-    (async-shell-command command "*pre-commit*")
-    ))
+    (async-shell-command command "*pre-commit*")))
