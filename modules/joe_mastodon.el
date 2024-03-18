842347c666142f36d1e55d74d7e0f800e15db7f5
Joe Higton <draxil@gmail.com>
2024-03-18 22:25:10 +0000

    [mastodon] pretty!

--- modules/joe_mastodon.el
+++ modules/joe_mastodon.el
@@ -1,6 +1,14 @@
-(use-package mastodon
-  :straight t
-  :defer 2
-  :config
-  (setq mastodon-instance-url "https://social.linux.pizza")
-  (setq mastodon-active-user "draxil"))
+(use-package
+ mastodon
+ :straight t
+ :defer 2
+ :config
+ (setq mastodon-instance-url "https://social.linux.pizza")
+ (setq mastodon-active-user "draxil"))
+
+(use-package
+ mastodon-alt
+ :straight
+ `(mastodon-alt :type git :host github :repo "rougier/mastodon-alt")
+ :after mastodon
+ :config (mastodon-alt-tl-activate))
