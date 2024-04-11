0b513294a270fa39800d702fff7169c295c838a2
Joe Higton <draxil@gmail.com>
2024-04-11 18:03:57 +0100

    [go] autoformat

--- modules/joe_go.el
+++ modules/joe_go.el
@@ -1,58 +1,55 @@
 
-(defvar rinj-go-mode nil "are we in ri-injector type project?")
+(defvar rinj-go-mode nil
+  "are we in ri-injector type project?")
 
-(use-package go-mode
-  :straight t
-  :config
-  (add-hook 'before-save-hook #'gofmt-before-save)
-  (use-package gotest
-    :straight t
-    :config
-    )
-  (setq-local show-trailing-whitespace t)
-  :hook
-  (go-mode . (lambda()
-	       (display-line-numbers-mode 1)
-	       (flyspell-prog-mode)
-	       (joe-go-test-all-tags)
-	       ;; TRYING INSTEAD OF ORIGAMI
-	       (yafolding-mode)
+(use-package
+ go-mode
+ :straight t
+ :config
+ (add-hook 'before-save-hook #'gofmt-before-save)
+ (use-package gotest :straight t :config)
+ (setq-local show-trailing-whitespace t)
+ :hook
+ (go-mode
+  .
+  (lambda ()
+    (display-line-numbers-mode 1)
+    (flyspell-prog-mode)
+    (joe-go-test-all-tags)
+    ;; TRYING INSTEAD OF ORIGAMI
+    (yafolding-mode)
 
-	       ;; make sure golangci-lint is run after the lsp checker
-	       (if (bound-and-true-p lsp-mode)
-		   (flycheck-add-next-checker
-		    'lsp
-		    'golangci-lint)
-		 )
+    ;; make sure golangci-lint is run after the lsp checker
+    (if (bound-and-true-p lsp-mode)
+        (flycheck-add-next-checker 'lsp 'golangci-lint))
 
-	       ;; this should be done by gomode IMO, make goimports a safe choice
-	       ;; for gofmt, we use this in some dir-locals for projects that need this
-	       (add-to-list 'safe-local-variable-values
-			    '(gofmt-command . "goimports"))
-	       ))
-  :bind (
-	 :map go-mode-map
-	      (
-	       ("C-c t t" . joe-go-test-current-project)
-	       ("C-c t f" . go-test-current-test)
-	       ("C-c t u" . joe-go-test-unit-mode)
-	       ("M-s f" . (lambda()
-			    (interactive)
-			    (consult-line "func ")))
-	       )))
+    ;; this should be done by gomode IMO, make goimports a safe choice
+    ;; for gofmt, we use this in some dir-locals for projects that need this
+    (add-to-list
+     'safe-local-variable-values '(gofmt-command . "goimports"))))
+ :bind
+ (:map
+  go-mode-map
+  (("C-c t t" . joe-go-test-current-project)
+   ("C-c t f" . go-test-current-test)
+   ("C-c t u" . joe-go-test-unit-mode)
+   ("M-s f" .
+    (lambda ()
+      (interactive)
+      (consult-line "func "))))))
 
 (defun joe-go-test-current-project ()
   "Run tests in an appropreate way."
   (interactive)
   (if rinj-go-mode
-    (rinj-ginkgo)
-    (go-test-current-project)
-  ))
+      (rinj-ginkgo)
+    (go-test-current-project)))
 
 ;; TODO make this a toggle
 (defun joe-go-test-all-tags ()
   (interactive)
-  (setq-local go-test-args "-tags=unit,integration,unit_tests,integ_tests"))
+  (setq-local go-test-args
+              "-tags=unit,integration,unit_tests,integ_tests"))
 
 (defun joe-go-test-no-tag-mode ()
   (interactive)
@@ -82,7 +79,8 @@
   (interactive)
   (kill-new (chomp (shell-command-to-string "go list"))))
 
-(use-package ginkgo-mode
-  :straight (ginkgo-mode :type git :host github :repo "garslo/ginkgo-mode")
-  :custom
-  (ginkgo-flags "-tags=unit" "run unit tests by default"))
+(use-package
+ ginkgo-mode
+ :straight
+ (ginkgo-mode :type git :host github :repo "garslo/ginkgo-mode")
+ :custom (ginkgo-flags "-tags=unit" "run unit tests by default"))
