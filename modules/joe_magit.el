061a5c3146d40db22ddc105b66aa779dbc2ab58d
Joe Higton <draxil@gmail.com>
2024-06-20 13:59:54 +0100

    [magit] autofmt gardening

--- modules/joe_magit.el
+++ modules/joe_magit.el
@@ -1,10 +1,11 @@
-(use-package magit
-  :straight t
-  :config
-  ;; highlight diff inside lines
-  (customize-set-value 'magit-diff-refine-hunk 'all)
-;;					:straight t
-  )
+(use-package
+ magit
+ :straight t
+ :config
+ ;; highlight diff inside lines
+ (customize-set-value 'magit-diff-refine-hunk 'all))
+
+(use-package transient :straight t)
 
 (defun joe-git-commit-mode-hook ()
   "When opening a commit, do this"
@@ -14,6 +15,6 @@
   (when (not (word-at-point))
     (let ((bn (magit-get-current-branch)))
       (when (string-match "^\\([A-Z]\\{3\\}-[0-9]+\\)" bn)
-	(insert (format "%s " (match-string 1 bn)))))))
+        (insert (format "%s " (match-string 1 bn)))))))
 
 (add-hook 'git-commit-setup-hook 'joe-git-commit-mode-hook)
