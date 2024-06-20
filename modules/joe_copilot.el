ae1619460e420e4179b0d62bb6339c5c9d407eff
Joe Higton <draxil@gmail.com>
2024-06-20 13:55:16 +0100

    [copilot] new function to shut up the copilot.

--- modules/joe_copilot.el
+++ modules/joe_copilot.el
@@ -13,4 +13,26 @@
 (define-key
  copilot-completion-map (kbd "C-[") 'copilot-previous-completion)
 
-(add-hook 'go-mode-hook 'copilot-mode)
+(defvar joe-shutup-copilot 't
+  "set to stop copilot coming on")
+
+(defun joe-shutup-copilot ()
+  "shut up copilot! (you are annoying)"
+  (interactive)
+  (if (fboundp 'copilot-mode)
+      (copilot-mode -1))
+  (setq joe-shutup-copilot 't))
+
+(defun joe-wakeup-copilot ()
+  "wake up copilot after it's been in the sin-bin"
+  (interactive)
+  (setq joe-shutup-copilot 'nil)
+  (if (fboundp 'copilot-mode)
+      (copilot-mode 1)))
+
+;; load copilot for go, but only when it's not been switched off.
+(add-hook
+ 'go-mode-hook
+ (lambda ()
+   (unless joe-shutup-copilot
+     (copilot-mode))))
