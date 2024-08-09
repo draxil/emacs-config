1c1fa43d0922b1a63bbea42c414e06f4d4e5cfb4
Joe Higton <draxil@gmail.com>
2024-08-09 17:02:10 +0100

    [dirvish] some suggested keys

--- modules/joe_dirvish.el
+++ modules/joe_dirvish.el
@@ -3,6 +3,27 @@
 (use-package
  dirvish
  :straight t
- :config
- (setq dirvish-reuse-session nil)
- (dirvish-override-dired-mode))
+ :init (dirvish-override-dired-mode)
+ :config (setq dirvish-reuse-session nil)
+ :bind
+ (("C-c C-f" . dirvish-fd)
+  :map
+  dirvish-mode-map
+  ("a" . dirvish-quick-access)
+  ("f" . dirvish-file-info-menu)
+  ("y" . dirvish-yank-menu)
+  ("N" . dirvish-narrow)
+  ("^" . dirvish-history-last)
+  ("h" . dirvish-history-jump) ; remapped `describe-mode'
+  ;;  ("s" . dirvish-quicksort) ; remapped `dired-sort-toggle-or-edit'
+  ("v" . dirvish-vc-menu) ; remapped `dired-view-file'
+  ("TAB" . dirvish-subtree-toggle)
+  ("C-c p" . dirvish-peek-mode)
+  ("M-f" . dirvish-history-go-forward)
+  ("M-b" . dirvish-history-go-backward)
+  ("M-l" . dirvish-ls-switches-menu)
+  ("M-m" . dirvish-mark-menu)
+  ("M-t" . dirvish-layout-toggle)
+  ("M-s" . dirvish-setup-menu)
+  ("M-e" . dirvish-emerge-menu)
+  ("M-j" . dirvish-fd-jump)))
