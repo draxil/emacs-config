752333322a133c4bc23aaae1fca173c12adab284
Joe Higton <draxil@gmail.com>
2024-08-09 16:23:08 +0100

    [embark] fix embark!

--- modules/joe_embark.el
+++ modules/joe_embark.el
@@ -1,49 +1,49 @@
 
-(use-package embark
-  :straight t
-  
-  :bind (
-	 (
-	  ("C-." . embark-act)
-	  ("C-h B" . embark-bindings)
-	  )
-  )
+(use-package
+ embark
+ :straight t
 
-  :init
+ :bind ((("C-." . embark-act) ("C-h B" . embark-bindings)))
 
-  ;; Optionally replace the key help with a completing-read interface
-  (setq prefix-help-command #'embark-prefix-help-command)
+ :init
 
-  ;; https://karthinks.com/software/fifteen-ways-to-use-embark/
-  (eval-when-compile
-    (defmacro my/embark-ace-action (fn)
-      `(defun ,(intern (concat "my/embark-ace-" (symbol-name fn))) ()
-	 (interactive)
-	 (with-demoted-errors "%s"
-           (require 'ace-window)
-           (let ((aw-dispatch-always t))
-             (aw-switch-to-window (aw-select nil))
-             (call-interactively (symbol-function ',fn)))))))
+ ;; Optionally replace the key help with a completing-read interface
+ (setq prefix-help-command #'embark-prefix-help-command)
 
-  :config
-  ;; open things in ace
-  (define-key embark-file-map     (kbd "o") (my/embark-ace-action find-file))
-  (define-key embark-buffer-map   (kbd "o") (my/embark-ace-action switch-to-buffer))
-  (define-key embark-bookmark-map (kbd "o") (my/embark-ace-action bookmark-jump))
-  
-  ;; Hide the mode line of the Embark live/completions buffers
-  (add-to-list 'display-buffer-alist
-               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-                 nil
-                 (window-parameters (mode-line-format . none)))))
+ ;; https://karthinks.com/software/fifteen-ways-to-use-embark/
+ (eval-when-compile
+   (defmacro my/embark-ace-action (fn)
+     `(defun ,(intern (concat "my/embark-ace-" (symbol-name fn))) ()
+        (interactive)
+        (with-demoted-errors "%s"
+          (require 'ace-window)
+          (let ((aw-dispatch-always t))
+            (aw-switch-to-window (aw-select nil))
+            (call-interactively (symbol-function ',fn)))))))
+
+ :config
+ ;; open things in ace
+ (define-key
+  embark-file-map (kbd "o") (my/embark-ace-action find-file))
+ (define-key
+  embark-buffer-map (kbd "o") (my/embark-ace-action switch-to-buffer))
+ (define-key
+  embark-bookmark-map (kbd "o") (my/embark-ace-action bookmark-jump))
+
+ ;; Hide the mode line of the Embark live/completions buffers
+ (add-to-list
+  'display-buffer-alist
+  '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+    nil
+    (window-parameters (mode-line-format . none)))))
 
 
-;; ;; Consult users will also want the embark-consult package.
-(use-package embark-consult
-  :ensure t
-  :after (embark consult)
-  :demand t ; only necessary if you have the hook below
-  ;; if you want to have consult previews as you move around an
-  ;; auto-updating embark collect buffer
-  :hook
-  (embark-collect-mode . consult-preview-at-point-mode))
+;; Consult users will also want the embark-consult package.
+(use-package
+ embark-consult
+ :straight t
+ :after (embark consult)
+ :demand t ; only necessary if you have the hook below
+ ;; if you want to have consult previews as you move around an
+ ;; auto-updating embark collect buffer
+ :hook (embark-collect-mode . consult-preview-at-point-mode))
