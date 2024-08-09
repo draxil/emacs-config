7586f811f27fbaaf73c8dcdfa939652eddd92a53
Joe Higton <draxil@gmail.com>
2024-08-09 16:29:24 +0100

    tidy ignore, also don't store org-clock-save.el in git any more

--- org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-clock-save.el - HQ1950172 at <2023-08-12 Sat 21:17>
-(setq org-clock-stored-history '(("/Users/joe.higton/Nextcloud/org-roam/20221104102644-ri_on_call.org" . 921) ("/Users/joe.higton/Nextcloud/org-roam/20210104172517-pe_time.org" . 14781)))
