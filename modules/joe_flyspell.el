11849c9fb4284dcd0d03d2d053a951f9088dd59a
Joe Higton <draxil@gmail.com>
2024-08-10 21:22:03 +0100

    [flyspell] stop flyspell from treading on consult

--- modules/joe_flyspell.el
+++ modules/joe_flyspell.el
@@ -1,5 +1,6 @@
-(use-package flyspell
-  :defer 4
-  :config
-  (unbind-key "C-;" flyspell-mode-map)
-  )
+(use-package
+ flyspell
+ :defer 4
+ :config
+ (unbind-key "C-;" flyspell-mode-map)
+ (unbind-key "C-." flyspell-mode-map))
