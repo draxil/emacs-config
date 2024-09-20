31fea577d440aa44716ecdb9c9f8d17b6f117d7c
Joe Higton <draxil@gmail.com>
2024-09-20 10:20:44 +0100

    [lsp] formatting, and switch on for... python?!

--- modules/joe_lsp.el
+++ modules/joe_lsp.el
@@ -1,21 +1,23 @@
-(use-package lsp-mode
-  :straight t
-  :commands (lsp lsp-deferred)
-  :custom
-  (lsp-ui-doc-show-with-cursor nil "dont want popups with cursor")
-  (lsp-go-env '((GOFLAGS . "-tags=unit,integration,acceptance,integ_tests,unit_tests")) "want unit tests etc")
-  :hook (
-	 (go-mode . lsp)
-	 (lsp-mode . lsp-enable-which-key-integration)
-	 )
-  :config
-  (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
-  :bind (
-	 :map lsp-command-map (
-			       ("s" . lsp-describe-thing-at-point)
-			       ("d" . lsp-find-type-definition)
-			       )
-))
+(use-package
+ lsp-mode
+ :straight t
+ :commands (lsp lsp-deferred)
+ :custom
+ (lsp-ui-doc-show-with-cursor nil "dont want popups with cursor")
+ (lsp-go-env
+  '((GOFLAGS
+     . "-tags=unit,integration,acceptance,integ_tests,unit_tests"))
+  "want unit tests etc")
+ :hook
+ ((go-mode . lsp)
+  (lsp-mode . lsp-enable-which-key-integration)
+  (python-mode . lsp))
+ :config (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
+ :bind
+ (:map
+  lsp-command-map
+  (("s" . lsp-describe-thing-at-point)
+   ("d" . lsp-find-type-definition))))
 
 
 ;; Optional - provides fancier overlays.
