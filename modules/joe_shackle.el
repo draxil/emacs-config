c4261e6466f7f6fa5095e9544d739ba00b9498c0
Joe Higton <draxil@gmail.com>
2025-02-11 09:11:13 +0000

    [shackle] fix transient

--- modules/joe_shackle.el
+++ modules/joe_shackle.el
@@ -1,19 +1,20 @@
 ;; Shackle provides control over emacs "windows" and how they open.
-(use-package shackle
-  :straight t
-  :init
-  (shackle-mode)
-  :config
-  (setq shackle-rules '(
-			("*vertico-buffer*" :align below :popup t)
-			("Vertico" :align below :popup t :regexp t)
-			("*vterm*" :align below :popup t)
-			("*Go Test*" :align below :popup t)
-			("*Gofmt Errors*" :align below :popup t)
-			("magit-diff" :select nil :other t)
-			("Magit:" :same t :regexp t)
-			)
-	shackle-default-rule '(:other t)
-	shackle-default-align 'right
-	))
-
+(use-package
+ shackle
+ :straight t
+ :init (shackle-mode)
+ :config
+ (setq
+  shackle-rules
+  '(("*vertico-buffer*" :align below :popup t)
+    ("Vertico" :align below :popup t :regexp t)
+    ("*vterm*" :align below :popup t)
+    ("*Go Test*" :align below :popup t)
+    ("*Gofmt Errors*" :align below :popup t)
+    ("magit-diff" :select nil :other t)
+    ("Magit:" :same t :regexp t)
+    ;; note the space here is how it seems to come out!
+    (" *transient*" :align below :popup t)
+    ("*Warnings*" :ignore t))
+  shackle-default-rule '(:other t)
+  shackle-default-align 'right))
