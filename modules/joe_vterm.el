8715670ac2794f913c0703620a14e1bfd9fa6e9e
Joe Higton <draxil@gmail.com>
2025-02-11 09:10:24 +0000

    [vterm] tidy

--- modules/joe_vterm.el
+++ modules/joe_vterm.el
@@ -1,16 +1,18 @@
 
-(use-package vterm-toggle
-  :defer t
-  :bind
-  ("<f2>" . 'vterm-toggle)
-  ("C-<f2>". 'vterm-toggle-cd)
-  )
+(use-package
+ vterm-toggle
+ :straight t
+ :bind
+ ("<f2>" . 'vterm-toggle)
+ ("C-<f2>" . 'vterm-toggle-cd))
 
-(use-package vterm
-  :after vterm-toggle
-  :config
-  ; remove f2 from vterm, we want that for toggle
-  (define-key vterm-mode-map [f2] nil)
-  (define-key vterm-mode-map [(control return)] 'vterm-toggle-insert-cd)
-  (define-key vterm-mode-map [f3] 'describe-mode)
-  )
+(use-package
+ vterm
+ :after vterm-toggle
+ :straight t
+ :config
+ ; remove f2 from vterm, we want that for toggle
+ (define-key vterm-mode-map [f2] nil)
+ (define-key
+  vterm-mode-map [(control return)] 'vterm-toggle-insert-cd)
+ (define-key vterm-mode-map [f3] 'describe-mode))
